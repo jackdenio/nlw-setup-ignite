c5b0a689cf6d6ab4047c6f676e49ab7ad42020ea
jackdenio <deniogomes@gmail.com>
2023-01-19 20:50:31 -0300

    Aula 02 - Avançando o back-end e front-end

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {  StatusBar } from 'react-native';
 import { 
   useFonts, 
   Inter_400Regular, 
@@ -9,6 +9,7 @@ import {
 
 import { Loading } from "./src/components/Loading";
 import { DebugInstructions } from "react-native/Libraries/NewAppScreen";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,24 +26,11 @@ export default function App() {
   }
 
  return (
-  <View style={styles.container}>
-    <Text style={styles.text}>setup denio.dev</Text>
+  <>
+    <Home />
     <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-  </View>
+  </>
  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-  
-});
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,21 @@
 import './styles/global.css';
-import { Habit } from "./components/Habit";
 
-function App() {
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
+// import { Habit } from "./components/Habit";
+
+export function App() {
     return (
-      <div>
-      <Habit completed={3}/>
-      <Habit completed={10}/>    
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+          <Header />
+          <SummaryTable/>
+          
+        </div>
+ 
       </div>
 
   )
 }
 
-export default App
-
-// Componente: Reaproveitar / isolar 
-// Propriedade: Uma informação enviada para modificar um componente visual ou comportamentalmente
